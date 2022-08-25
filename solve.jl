3e5c4fb4bb4f225dec35c84002e63848cbb9df03
Greg Clinton <greg.clinton@gmail.com>
2022-08-25 11:26:59 -0700

    latest

--- solve.jl
+++ solve.jl
@@ -1,21 +1,17 @@
 module solve
+using Random
 
-n = 100
-
-let
-    global f, g
-    using Random
+function test()
+    n = 100
 
     Random.seed!(1234)
     A = randn(Float32, (200, n))
+    x = zeros(Float32, n)
 
-    f(x) = -sum(log.(1 .- A * x)) - sum(log.(1 .- x .* x))
-    g(x) = -sum(1 ./ (1 .- A * x))
-end
-
-function test()
-    x0 = zeros(Float32, n)
-    println(f(x0), ' ', g(x0))
+    for _ in 1 : 1000 
+        val = -sum(log.(1 .- A * x)) - sum(log.(1 .- x .* x))
+        grad = 1 ./ (1 .- A * x)
+    end
 end
 
 test()
