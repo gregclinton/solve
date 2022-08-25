e1907705fc69b4f131d4335e2758c245b5e07be9
Greg Clinton <greg.clinton@gmail.com>
2022-08-25 08:27:52 -0700

    latest

--- solve.jl
+++ solve.jl
@@ -2,15 +2,19 @@ module solve
 
 using Random
 
+Random.seed!(1234)
+n = 50
+m = 100
+A = randn(Float32, (m, n))
+b = rand(Float32, m)
+
+function f(x)
+    sum(A * x)
+end
+
 function test()
-    Random.seed!(1234)
-
-    n = 50
-    m = 100
-    A = randn(Float32, (m, n))
-    b = rand(Float32, m)
-
-    println(sum(b), ' ', sum(A))
+    x0 = rand(Float32, n)
+    println(f(x0))
 end
 
 test()
