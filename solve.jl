4bec7fd2f2db36c577788af30df7753a54b6233f
Greg Clinton <greg.clinton@gmail.com>
2022-08-25 09:26:16 -0700

    latest

--- solve.jl
+++ solve.jl
@@ -3,17 +3,16 @@ module solve
 using Random
 
 Random.seed!(1234)
-n = 50
-m = 100
+n = 100
+m = 200
 A = randn(Float32, (m, n))
-b = rand(Float32, m)
 
 function f(x)
-    sum(A * x)
+    sum(ones(Float32, m) - A * x)
 end
 
 function test()
-    x0 = rand(Float32, n)
+    x0 = zeros(Float32, n)
     println(f(x0))
 end
 
