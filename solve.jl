6cbf21fdbdfa5a440b791e10477db7c7bcfc4dce
Greg Clinton <greg.clinton@gmail.com>
2022-08-26 08:37:45 -0700

    latest

--- solve.jl
+++ solve.jl
@@ -9,18 +9,24 @@ function test()
     β = 0.5
 
     for iters in 1 : 10000
-        val = -sum(log.(1 .- A * x)) - sum(log.(1 .- x .^ 2))
-        grad = A' * (1 ./ (1 .- A * x)) - 1 ./ (1 .+ x) + 1 ./ (1 .- x)
-        if norm(grad) < 1e-3
+        val = -sum(log.(1 .- A * x)) - sum(log.(1 .+ x)) - sum(log.(1 .- x))
+        d = 1 ./ (1 .- A * x)
+        grad = A' * d - 1 ./ (1 .+ x) + 1 ./ (1 .- x)
+        hess = A' * Diagonal(d .^ 2) * A + Diagonal(1 ./ (1 .+ x) .^ 2 + 1 ./ (1 .- x) .^ 2)
+        v = -hess\grad
+        fprime = grad' * v
+
+        if abs(fprime) < 1e-8
             println(iters)
             break
         end
-        v = -grad
-        fprime = grad' * v
+
         t = 1
+
         while maximum(A * (x + t * v)) ≥ 1 || maximum(abs.(x + t * v)) ≥ 1
             t *= β
         end
+
         while -sum(log.(1 .- A * (x + t * v))) - sum(log.(1 .- (x + t * v) .^ 2)) > val + α * t * fprime
             t *= β
         end
