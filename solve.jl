1a1c8ab41763cb56b4a4fb7a1c71c306d1afdddb
Greg Clinton <greg.clinton@gmail.com>
2022-08-26 07:27:12 -0700

    latest

--- solve.jl
+++ solve.jl
@@ -2,19 +2,28 @@ module solve
 using Random, LinearAlgebra
 
 function test()
-    n = 100
-
     Random.seed!(1234)
-    A = randn(Float32, (200, n))
-    x = zeros(Float32, n)
+    x = zeros(Float32, 100)
+    A = randn(Float32, (200, length(x)))
 
+    α = 0.01
+    β = 0.5
     for _ in 1 : 1000 
-#        val = -sum(log.(1 .- A * x)) - sum(log.(1 .- x .* x))
+        print('.')
+        val = -sum(log.(1 .- A * x)) - sum(log.(1 .- x .* x))
         grad = A' * (1 ./ (1 .- A * x)) - 1 ./ (1 .+ x) + 1 ./ (1 .- x)
         if norm(grad) < 1e-3
+            print('!')
             break
         end
         v = -grad
+        fprime = grad' * v
+        t = 1
+        while maximum(A * (x + t * v)) ≥ 1 || maximum(abs.(x + t * v)) ≥ 1
+            t *= β
+        end
+        t = 0
+        x += t * v
     end
 end
 
