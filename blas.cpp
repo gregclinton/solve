1f6b9e8c468a75e17d20815725dccc4a9b77c842
Greg Clinton <greg.clinton@gmail.com>
2022-08-26 14:41:34 -0700

    latest

--- blas.cpp
+++ blas.cpp
@@ -1,7 +1,7 @@
 /*
 sudo apt-get install libopenblas-dev
 
-g++ -std=c++20 -o test blas.cpp
+g++ -std=c++20 -o test blas.cpp -lopenblas
 */
 
 #include <iostream>
@@ -19,10 +19,17 @@ int main()
     mt19937 rng(1234);
     uniform_real_distribution<double> dist;
 
-    vector<double> a(10);
-    generate(begin(a), end(a), [&rng, &dist] () { return dist(rng); });
+    auto m = 2;
+    auto n = 2;
+    vector<double> A(m * n);
+    vector<double> B(m * n);
+    vector<double> C(m * n);
 
-    cout << a[0] << endl;
+    generate(begin(A), end(A), [&rng, &dist] () { return dist(rng); });
+    generate(begin(B), end(B), [&rng, &dist] () { return dist(rng); });
 
+    cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, m, n, n, 1, &A[0], n, &B[0], n, 0, &C[0], n);
+
+    cout << C[0] << endl;
     return 0;
 }
