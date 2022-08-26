8ef373d745f0cc1fbd7eb8b3d371303175ca78b8
Greg Clinton <greg.clinton@gmail.com>
2022-08-26 13:46:29 -0700

    latest

--- blas.cpp
+++ blas.cpp
@@ -5,6 +5,7 @@ g++ -std=c++20 -o test blas.cpp
 */
 
 #include <iostream>
+#include <random>
 #include <cblas.h>
 
 using namespace std;
@@ -12,5 +13,12 @@ using namespace std;
 int main()
 {
     cout << "blas" << endl;
+
+    random_device dev;
+    mt19937 rng(dev());
+
+    uniform_int_distribution<mt19937::result_type> dist6(1,6);
+    cout << dist6(rng) << endl;
+
     return 0;
 }
