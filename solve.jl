f316b7cf21ce34ee200aa3865d28b133018fa2d3
Greg Clinton <greg.clinton@gmail.com>
2022-08-27 07:34:52 -0700

    latest

--- solve.jl
+++ solve.jl
@@ -1,22 +1,16 @@
 module solve
 using Random, LinearAlgebra
 
-function test()
-    Random.seed!(1234)
-    x = zeros(Float32, 100)
-    A = randn(Float32, (200, length(x)))
+function newton(f, fable, grad, hess, x)
     α = 0.01
     β = 0.5
-    f(x) = -sum(log.(1 .- A * x)) - sum(log.(1 .+ x)) - sum(log.(1 .- x))
-    fable(x) = maximum(A * (x)) < 1 && maximum(abs.(x)) < 1 
 
     for iters in 1 : 10000
         val = f(x)
-        d = 1 ./ (1 .- A * x)
-        grad = A' * d - 1 ./ (1 .+ x) + 1 ./ (1 .- x)
-        hess = A' * Diagonal(d .^ 2) * A + Diagonal(1 ./ (1 .+ x) .^ 2 + 1 ./ (1 .- x) .^ 2)
-        v = -hess \ grad
-        fprime = grad' * v
+        H = hess(x)
+        g = grad(x);
+        v = -(H \ g)
+        fprime = g' * v
 
         if abs(fprime) < 1e-8
             println(iters)
@@ -31,6 +25,18 @@ function test()
 
         x += t * v
     end
+end
+
+function test()
+    Random.seed!(1234)
+    x = zeros(Float32, 100)
+    A = randn(Float32, (200, length(x)))
+    f(x) = -sum(log.(1 .- A * x)) - sum(log.(1 .+ x)) - sum(log.(1 .- x))
+    fable(x) = maximum(A * (x)) < 1 && maximum(abs.(x)) < 1
+    d(x) = 1 ./ (1 .- A * x)
+    grad(x) = A' * d(x) - 1 ./ (1 .+ x) + 1 ./ (1 .- x)
+    hess(x) = A' * Diagonal(d(x) .^ 2) * A + Diagonal(1 ./ (1 .+ x) .^ 2 + 1 ./ (1 .- x) .^ 2)
+newton(f, fable, grad, hess, x)
 end
 
 test()
